3d37c752729907f9d3abfb3df56a25b295be909b
Fabian Herrera <jherrera-2018183@kinal.edu.gt>
2023-01-27 22:23:02 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+document.querySelector('.menu-btn').addEventListener('click',()=>{
+    document.querySelector('.nav-menu').classList.toggle('show');
+});
+
+
+ScrollReveal().reveal('.portada');
+ScrollReveal().reveal('.pelis-cards',{delay:500});
+ScrollReveal().reveal('.cards-banner',{delay:500});
+ScrollReveal().reveal('.cards-banner',{delay:500});
+lReveal().reveal('.cards-banner-two',{delay:500});
